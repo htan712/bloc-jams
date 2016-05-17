1feb02edf884d52afc97aa1b7ac41b531ec19690
htan712 <hock.tan712@gmail.com>
2016-05-17 13:07:29 -0700

    Assignment Check

--- scripts/album.js
+++ scripts/album.js
@@ -58,15 +58,23 @@ var setCurrentAlbum = function(album) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
 };
-
 // find path to return 'song-item-number'
 var findParentByClassName = function(element, targetClass) {
     if (element) {
+        debugger
         var currentParent = element.parentElement;
-        while (currentParent.className != targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
+        // if there's no parent element
+        if (currentParent.length == 0) {
+            alert ("No Parent found!");
+        } else if (currentParent.className != targetClass && currentParent.className !== null) {
+            currentParent = currentParent.parentElement
+            // if currentParent is not targetClass
+            if (currentParent != targetClass) {
+                alert ("No Parent found with that class name");
+            }
+        } else {
+           return currentParent; 
         }
-        return currentParent;
     }
 };
 
